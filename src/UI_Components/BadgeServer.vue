a19662a3e7481aed59250193017ac5d44581b7fa
Kaizeeh <53438670+Kaiizeh@users.noreply.github.com>
2023-09-26 01:24:07 +0200

    Initial commit

---
+++ src/UI_Components/BadgeServer.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(["serverName, image"]);
+defineEmits(["helloWorld"])
+
+</script>
+
+<template>
+    <div class="badge" @click="$emit('helloWorld')">
+        KS
+    </div>
+</template>
+
+<style>
+.badge {
+    width: 2rem;
+    height: 2rem;
+    background: blue;
+}
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import BadgeServer from "../UI_Components/BadgeServer.vue";
+
+</script>
+
+<template>
+    <aside class="sidebar bg-neutral-800">
+        <BadgeServer @helloWorld="console.log('Hello world !')"></BadgeServer>
+    </aside>
+</template>
+
+<style>
+.sidebar {
+    width: 5rem;
+    height: 100vh;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: .25rem;
+}
+</style>
